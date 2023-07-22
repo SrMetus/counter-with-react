38e993aa17d36a6c674b1f38d3b824ed66081f0b
SrMetus <s.gaggero87@gmail.com>
2023-07-22 19:54:39 -0400

    Definiendo los props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = (props) => {
+  console.log(props)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div className='container'>
+        <h1>{props.primero}</h1>
+        <h1>{props.segundo}</h1>
+        <h1>{props.tercero}</h1>
+      </div>
+    );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,11 +5,23 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+let totalTime = 0;
+
+
+setInterval(() => {
+  totalTime++;
+  let primero = totalTime % 10;
+  let segundo = Math.floor(totalTime / 10) % 10;
+  let tercero = Math.floor(totalTime / 100) % 10;
+
+    
+    root.render(<App 
+      primero={primero} 
+      segundo={segundo}
+      tercero={tercero}
+    />);
+  }, 1000)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
