389e8e24c04e142b65cc73ae3841acf0e587c5f3
SrMetus <s.gaggero87@gmail.com>
2023-07-24 21:56:36 -0400

    Colocando estilos

--- src/App.js
+++ src/App.js
@@ -4,12 +4,26 @@ const App = (props) => {
   console.log(props)
   return (
       <div className='container'>
-        <h1>{props.primero}</h1>
-        <h1>{props.segundo}</h1>
-        <h1>{props.tercero}</h1>
-        <h1>{props.cuarto}</h1>
-        <h1>{props.quinto}</h1>
-        <h1>{props.sexto}</h1>
+        <div className="row position">
+          <div className="col">
+            <h1>{props.primero}</h1>
+          </div>
+          <div className="col">
+            <h1>{props.segundo}</h1>
+          </div>
+          <div className="col">
+            <h1>{props.tercero}</h1>
+          </div>
+          <div className="col">
+            <h1>{props.cuarto}</h1>
+          </div>
+          <div className="col">
+            <h1>{props.quinto}</h1>
+          </div>
+          <div className="col">
+            <h1>{props.sexto}</h1>
+          </div>
+        </div>
       </div>
     );
 };
