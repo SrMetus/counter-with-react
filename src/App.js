2ae94321c36a0a9adb49c7ea468a70dbdc842f9b
SrMetus <s.gaggero87@gmail.com>
2023-08-03 17:16:40 -0400

    Proyecto Finalizado

--- src/App.js
+++ src/App.js
@@ -6,21 +6,24 @@ const App = (props) => {
       <div className='container'>
         <div className="row">
           <div className="col">
+            <h1><i class="fa-regular fa-clock"></i></h1>
+          </div>
+          <div className="col">
             <h1>{props.sexto}</h1>
           </div>
-          <div class="col">
+          <div className="col">
             <h1>{props.quinto}</h1>
           </div>
-          <div class="col">
+          <div className="col">
             <h1>{props.cuarto}</h1>
           </div>
-          <div class="col">
+          <div className="col">
             <h1>{props.tercero}</h1>
           </div>
-          <div class="col">
+          <div className="col">
             <h1>{props.segundo}</h1>
           </div>
-          <div class="col">
+          <div className="col">
             <h1>{props.primero}</h1>
           </div>
         </div>
